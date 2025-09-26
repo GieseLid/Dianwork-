cd4edb90219a288eb107937c9e7129acd9216d5a
Giese <Giese@NAUGHTYKID>
2025-09-26 10:33:11 +0800

    添加项目文件。

---
+++ text1.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char A[100];
+    while (1)
+    {
+        scanf("%s", A);
+
+        if (strcmp(A, "Dian") == 0) {
+            printf("2002\n");
+        }
+        else if (strcmp(A, "Quit") == 0) {
+            // 可以选择 return 0; 结束程序
+            return 0;
+        }
+        else {
+            printf("Error\n");
+        }
+    }
+    return 0;
+}
